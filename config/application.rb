6e1e7f5d81539c82ff4545d7927de42615b18aa5
dgalache <dangalaxe@gmail.com>
2016-05-15 14:19:45 -0300

    Configurando Teste Automatizado

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,12 @@ module RealComputerWeb
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.generators do |g|
+        g.view_specs false
+        g.helper_specs false
+        g.controller_specs false
+      end
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
